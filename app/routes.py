38795c2b0436bbfb7cbcfa74c7183308c7e1cf3e
paul meyers <pmkundika@gmail.com>
2025-07-16 18:10:04 +0200

    finished backend

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,27 @@ from app.utils.functions import audio_downloader, video_downloader, get_url_info
 
 download_bp = Blueprint('downloader', __name__, url_prefix='/downloader')
 
+QUALITY = ['1080', '720', '360']
+
 @download_bp.route('/', methods=['GET', 'POST'])
 def index():
-    
     form = URLForm()
 
-    url = form.link.data
+    url = form.link.data.strip()
+    if not url:
+        return redirect(url_for('index'))
 
     if form.validate_on_submit():
         info = get_url_info(url)
+        
         quality = request.form.get('quality')
-        action = request.form.get('action')
+        if quality not in QUALITY:
+            return redirect(url_for('index'))
 
+        action = request.form.get('action')
+        if not action:
+            return redirect(url_for('index'))
+        
         if action == "download":
             response = video_downloader(url, quality)
             print(response)
