f0929f6a8cf3ca9043565e441c8e771af87e9550
paul meyers <pmkundika@gmail.com>
2025-07-16 20:37:52 +0200

    Finished the ui

--- app/routes.py
+++ app/routes.py
@@ -4,28 +4,25 @@ from app.utils.functions import audio_downloader, video_downloader, get_url_info
 
 download_bp = Blueprint('downloader', __name__, url_prefix='/downloader')
 
-QUALITY = ['1080', '720', '360']
-
 @download_bp.route('/', methods=['GET', 'POST'])
 def index():
     form = URLForm()
 
-    url = form.link.data.strip()
-    if not url:
-        return redirect(url_for('index'))
-
     if form.validate_on_submit():
+        url = form.link.data.strip()
+        if not url:
+            return redirect(url_for('downloader.index'))
+
         info = get_url_info(url)
         
         quality = request.form.get('quality')
-        if quality not in QUALITY:
-            return redirect(url_for('index'))
 
         action = request.form.get('action')
-        if not action:
-            return redirect(url_for('index'))
-        
+
         if action == "download":
+            if not quality:
+                return redirect(url_for('downloader.index'))
+            
             response = video_downloader(url, quality)
             print(response)
         return render_template('index.html', form=form, info=info)
