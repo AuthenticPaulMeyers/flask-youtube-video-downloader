fb3275b6611aa20befae4ef90aebf33e222134a7
paul meyers <pmkundika@gmail.com>
2025-08-07 14:00:05 +0200

    added close pop up button on error messages

--- run.py
+++ run.py
@@ -4,7 +4,3 @@ app = create_app()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
-
-
-#  <img src="{{ info['thumbnail'] }}" alt="Video thumbnail" class="rounded-t-lg">
-# <h2 class="m-2 text-xl font-semibold tracking-tight">{{ info['video_title'] }}</h2>
